8d16d5a2ba3a98bccf06c11a24763c7379f9c792
Bidhu Dash <bbd.ppun@gmail.com>
2021-05-20 00:13:59 +0530

    client end update

--- Employee Application Project/ClientSideEmployee/src/app/app-routing.module.ts
+++ Employee Application Project/ClientSideEmployee/src/app/app-routing.module.ts
@@ -6,16 +6,15 @@ import { EmployeeListComponent } from './employee-list/employee-list.component';
 import { UpdateEmployeeComponent } from './update-employee/update-employee.component';
 
 const routes: Routes = [
-{path:'',redirectTo:'employee',pathMatch:'full'},
-{path:'employees', component:EmployeeListComponent},
+{path:'**', redirectTo:'employee', pathMatch:'full'},
+{path:'employees', component :EmployeeListComponent},
 {path: 'add', component:CreateEmployeeComponent},
 {path:'update/:id',component:UpdateEmployeeComponent},
 {path:'details/:id', component:EmployeeDetailsComponent}
-
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
-})
+}) 
 export class AppRoutingModule { }
